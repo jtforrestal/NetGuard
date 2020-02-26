9019cdc66d3e96cc9409dbf3274151a14747e83e
Jack Forrestal <jforrest@tcd.ie>
2020-02-26 21:52:46 +0000

    Allows notification to olnly pull from list of non system apps, added dummy button

--- app/src/main/java/eu/faircode/netguard/ServiceForegroundMonitor.java
+++ app/src/main/java/eu/faircode/netguard/ServiceForegroundMonitor.java
@@ -6,34 +6,43 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.accessibility.AccessibilityEvent;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.core.app.NotificationCompat;
 
 
 public class ServiceForegroundMonitor extends AccessibilityService {
+    private static final String TAG = "NetGuard.Accesibility";
     private static final int NOTIFY_MONITORING =100;
-
+    private List<Rule> listRule  = new ArrayList<>();
     private NotificationManager nm;
     @Override
     public void onServiceConnected(){
         Notification notification = getInteractiveNotification("Home");
         nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         nm.notify(NOTIFY_MONITORING, notification);
+        listRule = Rule.getRules(true, ServiceForegroundMonitor.this);
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+
         CharSequence pkgName = event.getPackageName();
-        if(pkgName == "com.android.systemui"){
-            Notification notification = getInteractiveNotification("E");
-            nm.notify(NOTIFY_MONITORING, notification);
-            return;
-        }else {
-            Notification notification = getInteractiveNotification(pkgName);
-            nm.notify(NOTIFY_MONITORING, notification);
+
+        if(pkgName != null) {
+            Log.d(TAG,"Event, Package Name =" + pkgName.toString());
+            Rule rule = findRuleByPackageName(pkgName.toString(), listRule);
+            if (rule != null && !(rule.system)) {
+                Notification notification = getInteractiveNotification(rule.name);
+                nm.notify(NOTIFY_MONITORING, notification);
+
+            }
         }
     }
 
@@ -41,7 +50,11 @@ public class ServiceForegroundMonitor extends AccessibilityService {
     public void onInterrupt(){
     }
 
-
+    @Override
+    public boolean onUnbind(Intent intent){
+        nm.cancel(NOTIFY_MONITORING);
+        return false;
+    }
 
 
     private Notification getInteractiveNotification(CharSequence name ){
@@ -50,11 +63,13 @@ public class ServiceForegroundMonitor extends AccessibilityService {
         TypedValue tv = new TypedValue();
         getTheme().resolveAttribute(R.attr.colorPrimary, tv, true);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "foreground");
-        builder.setSmallIcon(R.drawable.ic_security_white_24dp)
+        builder.setSmallIcon(R.drawable.ic_equalizer_white_24dp)
                 .setContentIntent(pi)
                 .setColor(tv.data)
                 .setOngoing(true)
-                .setAutoCancel(false);
+                .setAutoCancel(false)
+                .addAction(R.drawable.ic_security_white_24dp, getString(R.string.foreground_rule_update), pi);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
             builder.setContentTitle(name);
 
@@ -70,6 +85,13 @@ public class ServiceForegroundMonitor extends AccessibilityService {
         return builder.build();
     }
 
-
+    private Rule findRuleByPackageName(String pck, List<Rule> lst){
+        for(Rule rule: lst){
+            if((rule.packageName != null) && rule.packageName.toLowerCase().contains(pck.toLowerCase())){
+                return rule;
+            }
+        }
+        return null;
+    }
 }
 
